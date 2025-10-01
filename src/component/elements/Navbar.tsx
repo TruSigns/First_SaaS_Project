208405a542428f040a886de80e724014e3db311d
Maurice Ruffin <Tysons3x3@gmail.com>
2025-09-30 21:25:56 -0400

    working on shared button and will continue tomorrow

--- src/component/elements/Navbar.tsx
+++ src/component/elements/Navbar.tsx
@@ -1,6 +1,10 @@
+import Container from "../shared/Container";
+import logo from "../../assets/ninja.svg";
+import { NavItem } from "../shared/NavItem";
+
 export const NavBar = () => {
   // create an array for different links and title
-  const navitems = [
+  const navItems = [
     {
       href: "#",
       text: "Home",
@@ -18,9 +22,34 @@ export const NavBar = () => {
       text: "Features",
     },
   ];
+
   return (
-    <header className="absolute inset-x-0 top-0 z-50 py-6"></header>
-  )
+    <header className="absolute inset-x-0 top-0 z-50 py-6">
+      <Container>
+        <nav className="w-full flex justify-between gap-6 relative">
+          {/* Logo  */}
+          <div className="min-w-max inline-flex relative">
+            <a href="/" className="relative flex items-center gap-3">
+              <img src={logo} className="w-20 h-20" alt="ninja-logo" />
+              <div className="inline-flex text-lg font-semibold text-heading-1">
+                Unknown Ninja
+              </div>
+            </a>
+          </div>
+
+          <div className="flex flex-col lg:flex-row w-full lg:justify-between lg:items-center absolute top-full left-0 lg:static lg:top-0 lg:bg-transparent border-x border-x-box-border lg:border-x-0 lg:h-auto h-0 overflow-hidden">
+            <ul className="border-t border-box-border lg:border-t-0 px-6 lg:px-0 pt-6 lg:pt-0 flex flex-col lg:flex-row gap-y-4 gap-x-3 text-lg text-heading-2 w-full lg:justify-center lg:items-center">
+              {navItems.map((item, key) => (
+                <NavItem href={item.href} text={item.text} key={key}/>
+              ))}
+            </ul>
+            <div className="lg:min-w-max flex items-center sm:w-max w-full pb-6 lg:pb-0 border-b border-box-border lg:border-0 px-6 lg:px-0"></div>
+              <BtnLink text="Get Started" href="#cta" className=""/>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  );
 };
 
 export default NavBar;
